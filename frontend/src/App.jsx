e050986505c4ce908592246355f71349e8b51e69
Aayushi Sharma <aayushisharma20@navgurukul.org>
2024-10-07 12:07:21 +0530

    Add Vercel File

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,14 @@
-// // App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import HomePage from "./components/HomePage";
-// import Login from "./components/Login";
-// import Signup from "./components/Signup";
-
-// function App() {
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/" element={<HomePage />} />
-//                 <Route path="/login" element={<Login />} />
-//                 <Route path="/signup" element={<Signup />} />
-//             </Routes>
-//         </Router>
-//     );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components//HomePage'; // Import HomePage component
-import ProductDetail from './components/ProductDetail'; // Import ProductDetail component
+import HomePage from './components//HomePage'; 
+import ProductDetail from './components/ProductDetail'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />  {/* Home Page ka route */}
-        <Route path="/product/:id" element={<ProductDetail />} />  {/* Product Detail ka route */}
+        <Route path="/" element={<HomePage />} />  
+        <Route path="/product/:id" element={<ProductDetail />} />  
       </Routes>
     </Router>
   );
